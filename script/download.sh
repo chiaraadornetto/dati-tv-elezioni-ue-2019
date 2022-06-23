6b9e83427c569287f45b96ec0d7e6d8928642568
Dennis Angemi <dennisangemi@gmail.com>
2022-06-23 23:19:37 +0000

    fix interventi.csv closes #1

--- script/download.sh
+++ script/download.sh
@@ -6,4 +6,4 @@ curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsaHHXb-DmqP9YkYat-9k
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsaHHXb-DmqP9YkYat-9k5sy9_N_wr2SIyASUZqQWjzRgLI-cm6b9U-sptcyz3Hp42LWltRQ2OKGx3/pub?gid=1270883097&single=true&output=csv" > ../data/trasmissioni.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsaHHXb-DmqP9YkYat-9k5sy9_N_wr2SIyASUZqQWjzRgLI-cm6b9U-sptcyz3Hp42LWltRQ2OKGx3/pub?gid=406960823&single=true&output=csv" > ../data/puntate.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsaHHXb-DmqP9YkYat-9k5sy9_N_wr2SIyASUZqQWjzRgLI-cm6b9U-sptcyz3Hp42LWltRQ2OKGx3/pub?gid=651724375&single=true&output=csv" > ../data/ospiti.csv
-curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsaHHXb-DmqP9YkYat-9k5sy9_N_wr2SIyASUZqQWjzRgLI-cm6b9U-sptcyz3Hp42LWltRQ2OKGx3/pub?gid=310866843&single=true&output=csv" > ../data/interventi.csv
+curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsaHHXb-DmqP9YkYat-9k5sy9_N_wr2SIyASUZqQWjzRgLI-cm6b9U-sptcyz3Hp42LWltRQ2OKGx3/pub?gid=325717862&single=true&output=csv" > ../data/interventi.csv
